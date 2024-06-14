fbfa0bc6db687aaa58016b6f54cf0ff34c02246d
PandaTrumpet <dsavontrumpet@gmail.com>
2024-06-14 18:59:30 +0300

    upda

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// console.log('JHello');
+// import { releaseYearRegexp } from './constants/movies-constants.js';
+// console.log(releaseYearRegexp.test('2000'));
+import startServer from './server.js';
+startServer();
---
+++ src/server.js
@@ -0,0 +1,86 @@
+// import express from "express";
+// import cors from "cors";
+// import pino from "pino-http";
+
+// import env from "./utils/env.js";
+
+// import movies from "./db/movies.js";
+
+// const port = env("PORT", "3000");
+
+// const startServer = ()=> {
+//     const app = express();
+
+//     const logger = pino({
+//         transport: {
+//             target: "pino-pretty"
+//         }
+//     });
+
+//     app.use(logger);
+//     app.use(cors());
+
+//     app.get("/api/movies", (req, res)=> {
+//         res.json(movies);
+//     })
+
+//     app.use((req, res)=> {
+//         res.status(404).json({
+//             message: "Not Found"
+//         })
+//     })
+
+//     app.listen(port, ()=> console.log(`Server running on ${port} PORT`))
+// }
+
+// export default startServer;
+
+// import express from 'express';
+// import cors from 'cors';
+// import movies from './db/movies.js';
+// const startServer = () => {
+//   const app = express();
+//   app.use(cors());
+
+//   app.get('/api/movies', (req, res) => {
+//     res.json(movies);
+//   });
+//   app.get((req, res) => {
+//     res.status(404).json({
+//       message: 'Not Found',
+//     });
+//   });
+//   app.listen(3000, () => {
+//     console.log('Server is runig 3000');
+//   });
+// };
+// export default startServer;
+
+import express from 'express';
+import cors from 'cors';
+import movies from './db/movies.js';
+import pino from 'pino-http';
+
+const startServer = () => {
+  const app = express();
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  });
+  app.use(cors());
+  app.use(logger);
+  app.get('/api/movies', (req, res) => {
+    res.json(movies);
+  });
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+  app.listen(3000, () => {
+    console.log('Server is runnig 3000');
+  });
+};
+
+export default startServer;
